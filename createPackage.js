906b21998e3f260035b728eaddced491efdb4d18
cdieckhoff <59147829+cdieckhoff@users.noreply.github.com>
2021-10-12 09:03:44 -0500

    Update createPackage.js

--- createPackage.js
+++ createPackage.js
@@ -2,5 +2,5 @@ function createPackage(small, big, goal) {
     // output the number of small bags needed
     // to ship the order
     let t = goal - (big * 5);
-    return t < 0 ? -1 : t;
+    return t>0 && t>s? -1 : t;
 }
